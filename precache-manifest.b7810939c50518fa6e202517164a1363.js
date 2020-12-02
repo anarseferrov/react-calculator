1c8aa4b779aa36ca21d70828981c9239bd628a57
Anar Seferov <1anarseferov@gmail.com>
2020-12-02 19:49:05 +0400

    Updates

---
+++ precache-manifest.b7810939c50518fa6e202517164a1363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646e2ecfaa0544991d26e6d30d1da460",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "3c8ccddb55a0e937e994",
+    "url": "/react-calculator/static/css/main.da8a228f.chunk.css"
+  },
+  {
+    "revision": "e6ef8e89708730d6c096",
+    "url": "/react-calculator/static/js/2.0c064290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.0c064290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8ccddb55a0e937e994",
+    "url": "/react-calculator/static/js/main.0a06934e.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
